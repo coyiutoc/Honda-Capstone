018df950ac08993b42a1c14ac0d22c91e014affc
Caitlin Coyiuto <coyiutoc@gmail.com>
2020-04-03 21:03:41 -0400

    Boilerplate + dependency setup.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import LandingPage from "./pages/LandingPage/LandingPage.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* <Navigation /> */}
+        <Switch>
+          <Route exact path = "/">
+            <LandingPage />     
+          </Route>
+        </Switch>
+        {/* </Footer /> */}
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import styles from './LandingPage.module.scss'; // Import css modules stylesheet as styles
+
+const LandingPage = () => {
+
+  return (
+    <div className={styles.landingPage}>
+      <h2>vroom vroom.</h2>
+      <p>Coming soon.</p>
+    </div>
+  );
+}
+
+export default LandingPage;
