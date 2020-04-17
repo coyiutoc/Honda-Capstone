f723fdd12e378e1f268da3fc032d6dc3c8903bd1
Caitlin Coyiuto <coyiutoc@gmail.com>
2020-04-17 13:12:47 -0400

    Some fixes to responsiveness + process pg start.

--- src/pages/ProcessPage/ProcessPage.js
+++ src/pages/ProcessPage/ProcessPage.js
@@ -1,12 +1,43 @@
-import React, {Component} from "react";
-import { ReactSVG } from 'react-svg'
+import React, {Component, useEffect, useRef} from "react";
+import { ReactSVG } from 'react-svg';
+import { HashLink } from 'react-router-hash-link';
 import styles from 'pages/ProcessPage/ProcessPage.module.scss';
 import star from 'assets/images/star.svg';
-// import data from 'data/bios.js';
+import data from 'data/process.js';
+import routes from "routes.js";
 
 import MediaQuery from 'react-responsive';
 
 const ProcessPage = () => {
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleNavigationHighlight);
+
+    return () => {
+      window.removeEventListener("scroll", handleNavigationHighlight);
+    }
+  });
+
+  function handleNavigationHighlight() {
+    let fromTop = window.scrollY;
+    let links = document.getElementById("navigationLinks").childNodes[0].children;
+    
+    for (let link of links) {
+      let title = link.innerHTML;
+      let section = document.getElementById(title);
+      if (
+        section.offsetTop <= fromTop &&
+        section.offsetTop + section.offsetHeight > fromTop
+      ) {
+        link.classList.add(styles.current);
+        link.classList.remove(styles.notCurrent);
+      } else {
+        link.classList.add(styles.notCurrent);
+        link.classList.remove(styles.current);
+      }
+    }
+  }
+
   return (
     <div>
       <div className={styles.header}>
@@ -15,45 +46,23 @@ const ProcessPage = () => {
         <ReactSVG className={styles.bgImg__squiggly} src={star} />
       </div>
       <div className={styles.content}>
-        <div className={styles.content__navigation}>
+        <div className={styles.content__navigation} id="navigationLinks">
           <div>
-          <a href="#">Project Brief</a>
-          <a href="#">Project Brief</a>
-          <a href="#">Project Brief</a>
-          <a href="#">Project Brief</a>
+            {
+              data.map((elem, index) => {
+                return <HashLink key={index} to={routes.process+"/#" + elem.sectionTitle}>{elem.sectionTitle}</HashLink>
+              })
+            }
           </div>
         </div>
         <div className={styles.content__body}>
-          <h3>Problem Brief</h3>
-          <p>
-          What We Started With
-
-          sjdghskdjghskdjghskd
-
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-          The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-          </p>
-          <h3>Problem Brief</h3>
-          <p>
-          What We Started With
-
-          sjdghskdjghskdjghskd
-
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-          The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-          </p>
-          <h3>Problem Brief</h3>
-          <p>
-          What We Started With
-
-          sjdghskdjghskdjghskd
-
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-          The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-          </p>
+          {data.map((elem, index) => {
+            return  <div key={index} id={elem.sectionTitle} className={styles.content__section}>
+                      <div className={styles.content__title}><h3>{elem.sectionTitle + " -"}</h3></div>
+                      <div className={styles.content__subtitle}>{elem.subtitle}</div>
+                      <div className={styles.content__text}>{elem.text}</div>
+                    </div>
+          })}
         </div>
       </div>
     </div>
