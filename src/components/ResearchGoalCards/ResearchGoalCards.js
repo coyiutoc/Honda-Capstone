df7f4cfb6c3a7a3e23a263b597fca6842ba418d1
Caitlin Coyiuto <coyiutoc@gmail.com>
2020-04-22 23:33:59 -0400

    Icons for research cards.

--- src/components/ResearchGoalCards/ResearchGoalCards.js
+++ src/components/ResearchGoalCards/ResearchGoalCards.js
@@ -1,14 +1,19 @@
 import React, {Component} from "react";
 import {Desktop, Tablet, Mobile, Default} from "components/MediaQueries.js";
 import styles from 'components/ResearchGoalCards/ResearchGoalCards.module.scss';
+import { ReactSVG } from 'react-svg';
+
+import r1 from 'assets/images/research1.svg';
+import r2 from 'assets/images/research2.svg';
+import r3 from 'assets/images/research3.svg';
 
 const data = [
   {text: "Understand how value is communicated through our stakeholders",
-   img: ""},
+   img: r3},
   {text: "Empathize with Design Thinking facilitators and 'implementors'",
-   img: ""},
+   img: r2},
   {text: "Identify pain points and opportunities within Design Thinking education",
-   img: ""},
+   img: r1},
 ]
 
 const ResearchGoalCards = () => {
@@ -18,7 +23,7 @@ const ResearchGoalCards = () => {
       {data.map((elem, index) => {
         return <div className={[styles.card, styles.boxShadow].join(' ')} key={index}>
                 <div className={styles.card__img}>
-                  icon here
+                  <ReactSVG src={elem.img} />
                 </div>
                 <div className={styles.card__text}>
                   {elem.text}
