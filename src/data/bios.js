2e436463af38b897b8f476d746fad514d653fbf3
Haley Park <haleypark.design@gmail.com>
2020-04-25 19:05:01 -0400

    just tweaked some data

--- src/data/bios.js
+++ src/data/bios.js
@@ -15,31 +15,31 @@ const data = {
   margot: {
     name: "Margot Henderson",
     position: "UX Research and Design, Co-Project Manager",
-    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    bio: "Margot is a designer coming from 6 years of work in graphic design and corporate art consulting. She often draws from her social psychology and fine arts academic background to inspire UX research and design choices. Outside of work, Margot always has a new sculpture or truffle recipe in the works.",
     imgURL: margotImg,
   },
   haley: {
     name: "Haley Park",
     position: "Design Lead",
-    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    bio: "Haley is a product designer passionate about design systems, UI animation, and mixed reality. She has worked at startups in Korea as a product designer. She also dabbles in web development, illustration, and brand design. She’s always down for karaoke and horror movies. To see some of her previous work, visit https://haleypark.design/",
     imgURL: haleyImg,
   },
   michael: {
     name: "Michael Silvestre",
     position: "UX Research and Design",
-    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    bio: "Michael is an aspiring UX Generalist with a background in community management and marketing for online gaming platforms. Self taught in Javascript, he also has an interest in front-end development. When it’s not too cold out, he likes to go on long-distance runs.",
     imgURL: michaelImg,
   },
   caitlin: {
     name: "Caitlin Coyiuto",
     position: "Technical Lead",
-    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    bio: "Caitlin is an aspiring UX Engineer with an interest in developer experience (DX), and has a background in Computer Science and Neuroscience. She has previously worked as an API developer, and has contributed to academic research ranging from collaborative VR systems to EEG studies on mood dysregulation. She spends most of her free time binge-watching cooking shows or hunting for good dim sum.",
     imgURL: caitlinImg
   },
   basil: {
     name: "Basil",
     position: "CEO, Cute Executive Officer",
-    bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    bio: "With three years experience in negotiating for treats, chicken, and head scratches, Basil oversees team wellness and evaluates the team for sufficient levels of cuteness. An outspoken member of the Pitt Crew, Basil always makes sure to get the last word in on Zoom meetings. ",
     imgURL: basilImg
   }
 }
--- src/data/mediumPosts.js
+++ src/data/mediumPosts.js
@@ -1,4 +1,6 @@
 import medium1 from "assets/images/medium/medium1.jpeg";
+import medium2 from "assets/images/medium/medium2.jpeg";
+import medium3 from "assets/images/medium/medium3.jpeg";
 
 const mediumPosts = [
   {
@@ -7,20 +9,20 @@ const mediumPosts = [
     img: medium1,
   },
   {
-    title: "Team Honda, Start Your Engines",
-    url: "https://medium.com/@matthewjohnfranklin/team-honda-start-your-engines-9c4e851a45e6",
-    img: medium1,
+    title: "Honda Kickoff 2020: A Pitt Stop in Columbus",
+    url: "https://medium.com/@mesilves/12a3fed8893f",
+    img: medium2,
   },
   {
-    title: "Team Honda, Start Your Engines",
-    url: "https://medium.com/@matthewjohnfranklin/team-honda-start-your-engines-9c4e851a45e6",
-    img: medium1,
+    title: "A Moment to Reflect",
+    url: "https://medium.com/@mesilves/17fec40d579f",
+    img: medium3,
   },
-  {
-    title: "Team Honda, Start Your Engines",
-    url: "https://medium.com/@matthewjohnfranklin/team-honda-start-your-engines-9c4e851a45e6",
-    img: medium1,
-  },
+  // {
+  //   title: "Team Honda, Start Your Engines",
+  //   url: "https://medium.com/@matthewjohnfranklin/team-honda-start-your-engines-9c4e851a45e6",
+  //   img: medium1,
+  // },
 ]
 
 export default mediumPosts;
